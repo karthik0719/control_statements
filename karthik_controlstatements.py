d57d63cbf2f0d923e902a95c5f322381feba951c
karthik0719 <104984916+karthik0719@users.noreply.github.com>
2022-05-30 17:11:33 +0530

    Add files via upload

---
+++ karthik_controlstatements.py
@@ -0,0 +1,39 @@
+'''Working with condition is control statements'''
+
+# for i in range(5):
+#     print(i,end="\n")
+
+# for i in range(1,6): to print from range 1 to 5
+#     print(i,end=" ")
+
+# The gap is called as skip or step
+
+# for i in range(1,10,2): To print odd number from 1 to 10
+#     print(i,end=" ")
+
+# for i in range(0,51,2): To print even numbers from 0 to 50 
+#      print(i,end=" ")
+
+# for i in range(10,-1,-1):
+#     print(i,end=" ")
+
+# s,e,sp=[int(x) for x in input("Enter s,e,sp values: ").split()]
+# for i in range(s,e,sp):
+#     print(i,end=" ")
+
+# a=[1,2,3.1,True,None]: for can be used as list, tupple
+# for i in a:
+#     print(i)
+
+'''While loop'''
+# i=1
+# while(i<=10):
+#     print("hi",i)
+#     i+=1
+
+start,end=[int(x) for x in input("Enter the values ").split()]
+while(start<=end):
+    print("ggmufc")
+    start+=2
+
+
